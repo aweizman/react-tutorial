b46c9070ba8e4464a27a90a340d4bfc1d8bcc0b2
Adam Weizman <adamweizman@gmail.com>
2020-08-03 15:54:47 -0400

    Finished main tic-tac-toe game
    Added ability to add X's and O's, ability to declare winner, turn counter. Need to fix being able to change existing squares. Need to stop players from playing when winner is declared. Need to complete tutorial to add "time-travel" feature

--- src/index.js
+++ src/index.js
@@ -2,27 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      turn: 0,
+      xIsNext: true, //starts off x first. can initialize to false to start with O. perhaps with button/switch?
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (this.state.turn <= 4) {
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      turn: ++this.state.turn,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    let turn;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    turn = 'Turn: ' + this.state.turn;
+    
     return (
       <div>
         <div className="status">{status}</div>
+        <div className="turn">{turn}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -57,6 +93,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
